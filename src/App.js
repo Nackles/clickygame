1bfd0f1cd1f5e8fc8d777c8d198dfee7f9c8fa9c
Nackles <45973617+Nackles@users.noreply.github.com>
2019-04-30 20:40:16 -0700

    images instantiating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+import Clickable from './components/Clickable.js';
+import characters from './characters.json';
 
-function App() {
+// Extend App as Component to use stateful variables
+class App extends Component {
+  // Initializing display variables and character cards from JSON 
+  state = {
+    combo: 0,
+    highest: 0,
+    characters
+  }
+
+
+  // Show a Jumbotron with instructions and a randomly sorted array of character images to click
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <Jumbotron>
+      <p>Clicking an image gives you a point, but only if you haven't clicked it yet... Try to click 12 in a row!</p>
+    </Jumbotron>
+    {/* For each character, create a card and track its clicked state */}
+    {this.state.characters.map(character => {
+      return <Clickable
+      key={character.id}
+      id={character.id}
+      img={character.img}
+      clicked={false}
+      />
+    })}
+    </Container>
   );
+ }
 }
 
 export default App;
---
+++ src/components/Clickable.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Clickable = props => (
+  <div>
+    <img src={props.img} alt={props.character} width={100} height={100}/>
+  </div>
+  )
+
+
+export default Clickable;
