bc0a20e36d4e238fbbfd13cf8135fbcf525e4fc8
Nackles <45973617+Nackles@users.noreply.github.com>
2019-05-02 10:33:24 -0700

    Updates

---
+++ precache-manifest.d674a47f6f35ef49c6e497b71787a1bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7562b7c66325ceab58666165475af0d3",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "cff299cebf978f73363e",
+    "url": "/clickygame/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d379a8deb6a2c20fc228",
+    "url": "/clickygame/static/css/main.6f8c9e61.chunk.css"
+  },
+  {
+    "revision": "cff299cebf978f73363e",
+    "url": "/clickygame/static/js/2.97daf7a9.chunk.js"
+  },
+  {
+    "revision": "d379a8deb6a2c20fc228",
+    "url": "/clickygame/static/js/main.708f832a.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
